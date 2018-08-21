4a621d126ea192805fa60ef6488b5e1066028181
Gadisa hy <thjeah@gmail.com>
2018-08-21 06:22:47 -0700

    my first git apps

---
+++ myFirstRepo/myFirstRepo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myFirstRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my cloned in Git hub");
+        }
+    }
+}
